a98583780bc06d12c999a416a5824c91d590bfc9
daian <daian@DESKTOP-J72HNCP>
2021-11-05 22:37:26 -0300

    segundo commit

--- src/teste/main.java
+++ src/teste/main.java
@@ -5,6 +5,19 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		
+		int corte = 2016;
+		int casamento = 2019;
+		int tempo = 0;
+			
+        tempo = subtracao (casamento, corte);
+        System.out.println(tempo);
+				
+		
+	}
+
+	private static int subtracao(int corte, int casamento) {
+		
+		return corte - casamento;
 	}
 
 }
